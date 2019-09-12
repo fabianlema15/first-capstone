25e007512c1da9af991e23e35f4d2a4bee5b2898
Fabian Lema <fabianlemac@gmail.com>
2019-09-12 12:57:56 -0500

    DB reference

--- seeds/seed-database.sql
+++ seeds/seed-database.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (user_code, first_name, last_name, address, phone, email, role, password)
-  VALUES (: 202033, 'Fabian', 'Lema', 'Here', '6128073412', 'fabianlemac@gmail.com', 'ADMIN', '$2a$08$UiectUc8z3BWevHi6CK2Z.FgIBGJnj9MLEvU1zW3ueu6Vn0RCmhna');
+  VALUES (202033, 'Fabian', 'Lema', 'Here', '6128073412', 'fabianlemac@gmail.com', 'ADMIN', '$2a$08$UiectUc8z3BWevHi6CK2Z.FgIBGJnj9MLEvU1zW3ueu6Vn0RCmhna');
